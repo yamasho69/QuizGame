60f3c9ca0fd680a27796162beffd4c3d6428aa24
yamasho69 <nitiham55@yahoo.co.jp>
2019-03-25 22:21:34 +0900

    QuizGame
    3回目のコミット、ランダム出題完成

--- Assets/Judge.cs
+++ Assets/Judge.cs
@@ -8,16 +8,25 @@ public class Judge : MonoBehaviour {
 
     public static Text selectedBtn;
 
+    GameObject quizmanager;
+    QuizMgr quizMgr;
+
     //選択したボタンのテキストラベルと正解のテキストを比較して正誤を判定
     public void CapText() {
+
+        quizmanager = GameObject.Find("QuizManager"); //QuizManagerをオブジェクトの名前から取得して変数に格納する
+        quizMgr = quizmanager.GetComponent<QuizMgr>();//Mgrはスクリプト、managerはオブジェクト
+
         //選択したボタンのテキストラベルを取得する
         Text selectedBtn = this.GetComponentInChildren<Text>();
-
+       
         //選択したボタンのテキストラベルと問題の答えを比較
         if (selectedBtn.text == QuizMgr.AnswerStr) {
             Debug.Log("正解");
+            quizMgr.Score += 10;
         } else {
             Debug.Log("不正解");
         }
+        quizMgr.NextQuizSet();//上記で作成したオブジェクトを使用する
     }
 }
--- Assets/QuizMgr.cs
+++ Assets/QuizMgr.cs
@@ -5,19 +5,19 @@ using UnityEngine.UI;//UI利用時必須
 using System.Linq;//配列に必要
 using System;//これがないとGuidクラスは使えない
 
-//http://ikedih.blog69.fc2.com/blog-entry-241.html
-
 public class QuizMgr: MonoBehaviour {
     public TextAsset csvFile;	　　　　　// GUIでcsvファイルを割当
 　　　　List<string[]> csvDatas = new List<string[]>(); //ここは参考ブログとは違う
     public static string AnswerStr;//問題の答え
     int[] Order1 = null; //出題数を管理するメンバ変数
-    int[] Order2;　//出題をランダムにするメンバ変数
-    public int Count = 1; //今何問目か
-
+    public int[] Order2;　//出題をランダムにするメンバ変数
+    public static int Count = 1; //今何問目か
+    public int Score = 0; //得点
 
 　　　　//スタート時、CSVファイルを読み込む
 　　　　public void Start() {
+
+        Judge judge = GetComponent<Judge>();
 
 　　　　　　　　// 格納
 　　　　　　　　string[] lines = csvFile.text.Replace("\r\n", "\n").Split("\n"[0]);
@@ -26,38 +26,27 @@ public class QuizMgr: MonoBehaviour {
             csvDatas.Add(line.Split(','));　　　　// string[]を追加している
 　　　　　　　　}
 
-　　　　　　　　// 書き出し
-　　　　　　　　//Debug.Log(csvDatas.Count);　　　　　　　　　// 行数
-　　　　　　　　//Debug.Log(csvDatas[0].Length);　　　　　　　// 項目数
-　　　　　　　　//Debug.Log(csvDatas[1][2]);        // 2行目3列目
-
-        this.Order1 = new int[csvDatas.Count];　//配列の要素数をCSVの行数分にする
+        Order1 = new int[csvDatas.Count];　//配列の要素数をCSVの行数分にする
         //配列に順番に数字を入れる
         for(int i = 0; i < csvDatas.Count-1; i++) {
             Order1[i] = i;
         }
         //配列の数字をランダムに入れ替えた配列を作成
-        this.Order2 = Order1.OrderBy(i => Guid.NewGuid()).ToArray();
-        //Debug.Log(Order1[33]); 
-        //Debug.Log(Order2[33]);
-
-
+        Order2 = Order1.OrderBy(i => Guid.NewGuid()).ToArray();
+        
         //問題をセットするメソッドを呼び出す
         QuizSet();
-
 　　　　}
     //問題をセットするメソッド
-    void QuizSet() {
+    void  QuizSet() {
 
         //1から4の配列(ary1)を作成
         int[] ary1 = new int[] {1, 2, 3, 4};
         //ary1をランダムに並び替えたary2を作成
         int[] ary2 = ary1.OrderBy(i => Guid.NewGuid()).ToArray();
 
-        for (int i =0; i < csvDatas.Count; i++) {
-
-            //答えをセット
-            AnswerStr = csvDatas[Order2[Count]][1];
+        //答えをセット
+        AnswerStr = csvDatas[Order2[Count]][1];
             //特定の名前のオブジェクトを検索してアクセス
             Text question = GameObject.Find("Quiz/Question").GetComponentInChildren<Text>();
             Text button1 = GameObject.Find("Quiz/Button1").GetComponentInChildren<Text>();
@@ -70,7 +59,11 @@ public class QuizMgr: MonoBehaviour {
             button2.text = csvDatas[Order2[Count]][ary2[1]];//ary2の２番目に入っている数字の項目をボタン２にセット
             button3.text = csvDatas[Order2[Count]][ary2[2]];//ary2の３番目に入っている数字の項目をボタン３にセット
             button4.text = csvDatas[Order2[Count]][ary2[3]];//ary2の４番目に入っている数字の項目をボタン４にセット
-        }
+    }
+    public void NextQuizSet() {
+        Count++;
+        Debug.Log(Count);
+        if(Count == 11) {
+        } else {QuizSet();}
     }
 }
-
