3714383c057f68b7b465c73fee85600ba7664d7d
yamasho69 <nitiham55@yahoo.co.jp>
2019-04-17 23:48:25 +0900

    12回目のコミット
    タイトル画面作成

---
+++ Assets/Shine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Coffee.UIExtensions;//追加
+
+//参考URL　コガネブログ　http://baba-s.hatenablog.com/entry/2018/05/21/090000
+
+public class Shine : MonoBehaviour {
+
+    public ShinyEffectForUGUI m_shiny;
+
+    private void Start() {
+        ShinePlay();
+    }
+    void ShinePlay() {
+        m_shiny.Play(1);
+        Invoke("ShinePlay", 2.0f);
+    }
+
+}
