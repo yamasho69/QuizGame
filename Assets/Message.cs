c660e350f3b187d0e85d692a24d0bd8bf967f7d8
yamasho69 <nitiham55@yahoo.co.jp>
2019-04-18 23:23:58 +0900

    13回目のコミット
    クレジット＆プライバシーポリシーウィンドウを実装

---
+++ Assets/Message.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//参考URL　https://freesworder.net/unity-setactive-true-false/
+
+public class Message : MonoBehaviour {
+
+    public GameObject MessageWindow;//メッセージウィンドウをゲームオブジェクト型で定義
+    // Use this for initialization
+
+    // Update is called once per frame
+    public void OffWindow() {
+            MessageWindow.SetActive(false);//オブジェクトを非表示に
+    }
+
+    public void OnWindow() {
+        MessageWindow.SetActive(true);//オブジェクトを表示する
+    }
+}
