c90ca049bbd72d933e7f484e3d55bdc8ee0e498e
yamasho69 <nitiham55@yahoo.co.jp>
2019-07-28 14:18:15 +0900

    26回目のコミット
    GooglePlayにアップロード

--- Assets/Script/AdMob.cs
+++ Assets/Script/AdMob.cs
@@ -11,7 +11,9 @@ public class AdMob : MonoBehaviour {
     void Start() {
 
         // アプリID、 これはテスト用
-        string appId = "ca-app-pub-3940256099942544~3347511713";
+        //string appId = "ca-app-pub-3940256099942544~3347511713";
+        //本番用
+        string appId = "ca-app-pub-3712506788718089~6263895603";
 
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize(appId);
@@ -22,7 +24,9 @@ public class AdMob : MonoBehaviour {
     private void RequestBanner() {
 
         // 広告ユニットID これはテスト用
-        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+        //string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+        //本番用
+        string adUnitId = "ca-app-pub-3712506788718089/1607363080";
 
         // Create a 320x50 banner at the top of the screen.
         BannerView bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
